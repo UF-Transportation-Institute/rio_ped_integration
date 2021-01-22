187dfc69027392759359d156f6444c4f92b4ec5e
lebedev <ylebedev@ufl.edu>
2021-01-22 15:11:16 -0500

    =integration

--- rio_integration/scripts/rio_integration_node.py
+++ rio_integration/scripts/rio_integration_node.py
@@ -2,18 +2,30 @@
 
 ## RIO Integration that listens to String published
 ## to the 'counts' topic
+
 import rospy
 from std_msgs.msg import String
+
+import socket
 
 
 class RioIntegration:
 
-    def callback(self, data):
-      print(data)
-
+    def callback(self, msg):
+        print(msg)
+        try:
+            self._bsm_publisher.sendto(msg.data, (self.ip, self.port))
+        except Exception:
+            # ok to ignore some counts
+            pass
 
     def __init__(self):
         rospy.Subscriber('ped_counter/counts', String, self.callback)
+        self.ip = rospy.get_param("rio_integration_node/server_ip")
+        self.port = rospy.get_param("rio_integration_node/server_port")
+
+        self._bsm_publisher = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._bsm_publisher.setblocking(0)
 
 
 if __name__ == '__main__':
